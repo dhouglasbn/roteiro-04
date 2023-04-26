452b6e2e2eb85a5a338f963c3f6ac622f8ae51ee
dhouglas.nobrega <dhouglas.nobrega@150.165.42.120>
2023-04-26 09:49:07 -0300

    first commit

---
+++ src/main/java/orderStatistic/Main.java
@@ -0,0 +1,14 @@
+package orderStatistic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Integer [] array = {4,8,6,9,12,1};
+		KLargestOrderStatisticsImpl<Integer> klargest = new KLargestOrderStatisticsImpl<Integer>();
+		Integer[] result = klargest.getKLargest(array, 3);
+		for(Integer n: result) {
+			System.out.println(n);
+		}
+	}
+
+}
